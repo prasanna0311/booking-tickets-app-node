1e8321416864b2bf3bb723cd76f29b20215126fd
Prasanna Balaji D <83944299+prasanna0311@users.noreply.github.com>
2021-12-10 21:34:20 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,27 +1,43 @@
-// const express = require("express");
-import cors from "cors";
-import express from "express";
-import mongoose from "mongoose";
-import { userRouter } from "./routes/users.js";
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+//  import dotenv from 'dotenv';
+import mongoose  from 'mongoose';
+
+//import Connection from './database/db.js';
+import Routes from './routes/Routes.js';
+
+
+//  dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 5000;
 
-const url = `mongodb+srv://prasanna:prasanna98@cluster0.nx947.mongodb.net/bookingtickets`;
-// ||
-// "mongodb://localhost/BookMyShow";
+const PORT = 8000;
 
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
-const con = mongoose.connection;
+const URL =`mongodb+srv://prasanna:prasanna98@cluster0.nx947.mongodb.net/whatsappclone?retryWrites=true&w=majority`;
+   
+    mongoose.connect(URL, { useNewUrlParser: true, useUnifiedTopology: true });
+    const con = mongoose.connection;
 con.on("open", () => console.log("MongoDB is connected"));
+
+
+
+// const username = "prasanna";
+// const password = "prasanna98";
+
+
+// Connection(username, password);
+
+app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
 
 // middleware
 app.use(express.json());
-app.use(cors());
+
 
 app.get("/", (request, respone) => {
-  respone.send("Welcome to bookmyshow!!!! Hi Guys");
+  respone.send("Welcome to whatsapp clone!!!! Hi Guys");
 });
 
-app.use("/users", userRouter);
-
-app.listen(PORT, () => console.log("The server is started in " + PORT));
+app.use(bodyParser.json({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.use('/', Routes);
